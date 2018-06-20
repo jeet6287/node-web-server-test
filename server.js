331823136a6c4137cd50fe784f59006ac109e164
Jitendre <jitendra.kumar@xseededucation.com>
2018-06-20 10:49:23 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var hbs = require('hbs')
+
+var app = express();
+
+hbs.registerPartials(__dirname +'/views/partials')
+app.set('viewEngine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res) => {
+    res.render('home.hbs',{
+      title:'home page',
+      currentYear: new Date().getFullYear(),
+      welcome:'Welcome to home page'
+    });
+});
+
+app.get('/about',(req,res) => {
+    res.render('about.hbs',{
+      title:'About page',
+      currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad',(req,res) => {
+    res.send({
+      error:'unable to load'
+    });
+});
+
+
+app.listen(3000,()=>{
+	console.log('server listen at port 3000.');
+});
